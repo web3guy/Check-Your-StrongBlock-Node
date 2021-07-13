283477604fbb649a2c0888819df34e0713f25a9b
web3guy <86728272+web3guy@users.noreply.github.com>
2021-07-14 02:14:35 +0530

    Update lets-test-the-node.py

--- lets-test-the-node.py
+++ lets-test-the-node.py
@@ -4,7 +4,8 @@ from web3 import Web3
 my_node_url = "https://mainnet.strongblock.com/1c6212813d5aacb0805051b3ca125badd878845e"
 
 
+#Don't thouch this one
+
 w3 = Web3(Web3.HTTPProvider(my_node_url))
 print(w3.isConnected())
 
-#Hurray! If everything went right, you will get 'True'  when you run this file.
